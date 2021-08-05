a2deeb4d0f2fda0107e0d4beed8e791dab1b21a4
NotABananaPeel <88512194+NotABananaPeel@users.noreply.github.com>
2021-08-05 13:51:24 -0700

    Add files via upload

---
+++ Project_Template_C10_Travelling_in_Ship-main/sketch.js
@@ -0,0 +1,17 @@
+var s
+function preload(){
+seaImg.loadImage("sea.png")
+shipImg.loadAnimation
+}
+
+function setup(){
+  createCanvas(400,400);
+  s.addImage(seaImg)
+}
+
+function draw() {
+  background("blue");
+ if(s.x<0){
+sea.x=width/2
+ }
+}
